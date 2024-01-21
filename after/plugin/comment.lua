6baac0a4f4d6d82db8c031181435f0eb976bbc3b
Martin Oliver Pitoňák <martin.pit18@gmail.com>
2024-01-21 18:54:11 +0100

    changed comment bindings

--- after/plugin/comment.lua
+++ after/plugin/comment.lua
@@ -1,13 +1,8 @@
-local api = require('Comment.api')
-
-vim.keymap.set('n', '<leader>l', api.call('toggle.line', '1'), { expr = true })
-
-vim.keymap.set(
-    'n', '<leader>c', api.call('toggle.linewise', 'g@'),
-    { expr = true }
-)
-
-vim.keymap.set(
-    'n', '<leader>b', api.call('toggle.blockwise', 'g@'),
-    { expr = true }
-)
+require('Comment').setup({
+    toggler = {
+        line = '<leader>\\',
+    },
+    opleader = {
+        block = '<leader>bc',
+    },
+})
