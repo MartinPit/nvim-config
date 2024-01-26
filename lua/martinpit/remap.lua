88cfec87b60b108b8de2ad16347015d665bd2549
Martin Oliver Pitoňák <martin.pit18@gmail.com>
2024-01-26 21:38:37 +0100

    added easy word replace

--- lua/martinpit/remap.lua
+++ lua/martinpit/remap.lua
@@ -28,4 +28,5 @@ end)
 
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", {silent = true})
 
+vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
 vim.api.nvim_set_keymap('i', '<M-CR>', 'copilot#Accept("<CR>")', {expr=true, silent=true})
