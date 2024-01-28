586ef13df278f22b66c27fb250718c00ec1cf213
Martin Oliver Pitoňák <martin.pit18@gmail.com>
2024-01-28 14:44:16 +0100

    added refactoring.nvim

---
+++ after/plugin/refactoring.lua
@@ -0,0 +1,7 @@
+require('refactoring').setup()
+
+
+vim.keymap.set({'n', 'x'}, '<leader>re', function() require('telescope').extensions.refactoring.refactors() end)
+vim.keymap.set("n", "<leader>rp", function() require('refactoring').debug.printf({below = false}) end)
+vim.keymap.set({"x", "n"}, "<leader>rv", function() require('refactoring').debug.print_var() end)
+vim.keymap.set("n", "<leader>rc", function() require('refactoring').debug.cleanup({}) end)
