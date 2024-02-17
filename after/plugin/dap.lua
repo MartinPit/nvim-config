1871d13a6bb574a6553ec99ff8b263c0fc331445
Martin Oliver Pitoňák <martin.pit18@gmail.com>
2024-02-17 15:00:36 +0100

    dap config for c++

--- after/plugin/dap.lua
+++ after/plugin/dap.lua
@@ -1,6 +1,27 @@
 local dapui = require('dapui')
 local dap = require('dap')
 
+dap.adapters.gdb = {
+    type = 'executable',
+    command = 'gdb',
+    args = { '-i', 'dap' }
+}
+
+dap.configurations.cpp = {
+    {
+        name = 'Launch',
+        type = 'gdb',
+        request = 'launch',
+        program = function()
+            return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+        end,
+        cwd = '${workspaceFolder}',
+        stopAtBeginningOfMainSubprogram = false,
+        stopOnEntry = false,
+        args = {},
+    }
+}
+
 vim.keymap.set('n', '<leader>dt', function() dapui.toggle() end, { noremap = true })
 vim.keymap.set('n', '<leader>bp', function() dap.toggle_breakpoint() end, { noremap = true })
 
